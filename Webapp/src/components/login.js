7a64755af342e8084065b2d23e3aff7efc842fd1
Aishwarya <surve.a@northeastern.edu>
2021-11-28 13:08:41 -0500

    Added scss for login

--- Webapp/src/components/login.js
+++ Webapp/src/components/login.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import '../Styles/login.scss';
 export const Login = (props)=>{
     return(
-      <div className = "signup-form">
-        <h3>WELCOME TO SPLITWISE</h3>
+      <div className='container'>
+        <div className = "login-form">
+        <h3>Log In</h3>
         <label htmlFor="">Email address</label>
         <input id = "email" onChange = {props.input} type="text"/>
         <label htmlFor="">Password</label>
         <input id = "password" onChange = {props.input} type="text"/>
-        <button onClick = {props.login} className = "btn">Log In</button>
-     </div>
+        <button onClick = {props.login} className = "login-btn">Log In</button>
+        </div>
+      </div>
+      
     )
 } 
