54cf2451fdfd80201de22ada0b5312dc51c7b9ab
Diksha Godse <godse.d@northeastern.edu>
2021-11-29 10:20:25 -0500

    added signup page

--- Server/api/utils/environment.js
+++ Server/api/utils/environment.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // mongo: 'mongodb+srv://admin:admin@shopdb-ykzx1.mongodb.net/Splitwise?retryWrites=true'
     mongo: 'mongodb://localhost:27017/splitwise_db'
 }
---
+++ Webapp/src/utilities/AxiosConfig.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+export const instance = axios.create({
+    baseURL: 'http://localhost:1234/',
+    timeout: 4000
+}
+);
+
+export default function setAuthorizationToken(token){
+    if(token){
+        instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+    }else{
+        delete instance.defaults.headers.common['Authorization'];
+    }
+}
