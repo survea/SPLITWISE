e9dec1cea6c916079ab6aeb1e5ed7a844030dc98
Rajiv Ranjan Sahu <sahu.raj@northeastern.edu>
2021-11-29 13:01:30 -0500

    Created landing page for the app

--- Webapp/src/App/App.js
+++ Webapp/src/App/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import {Login} from '../components/login';
 import SignUp from '../components/signup';
+import { Landing } from '../components/landing';
 
 export  class App extends React.Component{
   render(){
     return (
       <BrowserRouter>
       <Switch>  
+       <Route exact path = "/" component = {Landing}></Route>
           <Route exact path = "/login" component = {Login}></Route>
           <Route exact path = "/signup" component = {SignUp}></Route>
        </Switch>
