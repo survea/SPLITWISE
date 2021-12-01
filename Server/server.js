66575c3c008d10ffa8afed51987e5baa0a0a2110
Diksha Godse <godse.d@northeastern.edu>
2021-11-30 19:05:36 -0500

    Added user authentication to view user dashboard.

--- Server/server.js
+++ Server/server.js
@@ -7,11 +7,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use(require('./api/utils/cors'));
-
 app.use("/",userRoute);
 app.listen(process.env.PORT || 1234,(err)=>{
     if(err){
-        console.log("Error in sever Staring ",err);
+        console.log("Error in server Starting ",err);
     }else{
         console.log("Server started .....")
     }
--- Webapp/src/App/App.js
+++ Webapp/src/App/App.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import LoginContainer from '../containers/login';
-import {Dashboard} from '../components/dashboard';
+import { Dashboard } from '../components/dashboard';
 import SignUp from '../components/signup';
 import { Landing } from '../components/landing';
-export  class App extends React.Component{
-  render(){
+import AuthComp from '../containers/AuthComp';
+
+export class App extends React.Component {
+  render() {
     return (
       <BrowserRouter>
-      <Switch>
-          <Route exact path = "/login" component = {LoginContainer}></Route>
-          <Route exact path = "/dashboard" component = {Dashboard}></Route>
-          <Route exact path = "/" component = {Landing}></Route>
-          <Route exact path = "/signup" component = {SignUp}></Route>
-       </Switch>
+        <Switch>
+          <Route exact path="/login" component={LoginContainer}></Route>
+          <AuthComp>
+            <Route exact path="/dashboard" component={Dashboard}></Route>
+          </AuthComp>
+          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/signup" component={SignUp}></Route>
+        </Switch>
       </BrowserRouter>
-      
-  )
+
+    )
   }
 }
