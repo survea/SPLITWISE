00049f548c22c73f28be5a6bea604410c19a39e2
Diksha Godse <godse.d@northeastern.edu>
2021-12-03 17:37:28 -0500

    Webapp folder structure updated

---
+++ Webapp/src/containers/LoginContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Login } from '../components/LoginComponent/Login';
+import {instance} from '../utilities/AxiosConfig';
+import { withRouter } from "react-router-dom";
+ class LoginContainer extends React.Component{
+    constructor(props){
+        super(props)
+        this.props = props;
+        this.input = {};
+        this.state = {invalid: false};
+    }
+    TakeInput(event){
+        this.input[event.target.id] = event.target.value;
+    }
+    Login(){
+    var pr = instance.post('/login',this.input);
+
+    pr.then((response)=>{
+        console.log(response.data);
+        const token = response.data.token;
+        localStorage.setItem('jwtToken',token)
+        this.props.history.push("/dashboard");
+        if(response.data.Status === 'S'){
+            this.props.history.push("/dashboard");
+        }
+        else if(response.data.Status === 'F'){
+          this.setState({invalid:true});
+        }
+    })
+    }
+    render(){
+        return(
+        <Login sts = {this.state.invalid} input = {this.TakeInput.bind(this)} login = {this.Login.bind(this)}/>
+        )
+    }
+}
+
+export default withRouter(LoginContainer);
