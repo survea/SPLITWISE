61738a5bc0a58a02ecf0945a7d191b5a96425131
Rajiv Ranjan Sahu <sahu.raj@northeastern.edu>
2021-12-03 08:53:29 -0500

    Fixed the landing issue and added documentation

--- Webapp/src/components/landing.js
+++ Webapp/src/components/landing.js
@@ -1,25 +1,31 @@
+// Importing the required resources for the landing page
+
 import React from 'react';
 import '../Styles/landing.scss'
 import Header  from './Header';
-import Footer from './Footer';
+
 export const Landing = ()=>{
     return (
         <div className = "landing">
+
+            {/* Using header for the top nav landing page */}
+
            <Header/>
 
             <main >
+                {/* Creating the landing header part of the landing page */}
                 <div className = "landing-heading">
                 <h1 className = "landing-header">Less stress when sharing expenses with anyone.</h1>
                 <p className = "landing-desc"><strong>Share</strong> bills. And make sure the bills are <strong>splitted in the right way</strong> and <strong>settled-up</strong>.</p>
                 <p className = "image-desc">
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset1@2x.png" height="600" width="375" alt="Image features"/>
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset2@2x.png" height="600" width="375" alt="Image features"/>
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset3@2x.png" height="600" width="375" alt="Image features"/>
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset4@2x.png" height="600" width="375" alt="Image features"/>
+                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset1@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
+                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset2@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
+                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset3@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
+                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset4@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
                 </p>
                 
                 </div>
-                
+                {/* Creating the landing feature of the landing page */}
                 <div className = "landing-feature">
                     <div className = "landing-content">
                         <h1>Splitting expenses are now a piece of cake.</h1> 
@@ -35,7 +41,6 @@ export const Landing = ()=>{
                 </div> 
                
             </main>
-            
         </div>
     )
 }
