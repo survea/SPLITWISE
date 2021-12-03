ba30725b06920bdbd90853f23e79da7f932f88c7
Rajiv Ranjan Sahu <sahu.raj@northeastern.edu>
2021-12-03 18:26:32 -0500

    Refactored the cod and Added the footer and added the images

--- Webapp/src/components/Header.js
+++ Webapp/src/components/Header.js
@@ -1,13 +1,23 @@
+// Importing the required resources for the landing page
+
 import React from 'react'
 import {NavLink} from 'react-router-dom';
 import {connect} from 'react-redux';
+import logo from '../images/logo.png';
 
 import '../Styles/landing.scss'
+
+/**
+ * The header class takes care of the logo, login and signup pages
+ * @param {*} props 
+ * @returns 
+ */
+
  const Header = (props)=>{
     return (
         <nav className = "landingNav fixed-top">
         <NavLink to = "/">
-        
+        <img className="img-logo" src={logo} alt="SplitWise Logo"/>
         <h3 className = "landing-name">S P L I T W I S E</h3></NavLink> 
      
      <div className = "float">
@@ -15,9 +25,6 @@ import '../Styles/landing.scss'
         <label htmlFor="">or</label>
         <NavLink to = "/signup"><button className = "SignUp">Sign Up</button></NavLink>
      </div>
-        
-
-
     </nav>
     )
 }
@@ -28,6 +35,8 @@ const mapStateToProps = (state)=>{
        user: state.user
    }
 }
+
+// Exporting as default Header to the landing page
 
 const fn = connect(mapStateToProps);
 export default fn(Header);
--- Webapp/src/components/landing.js
+++ Webapp/src/components/landing.js
@@ -3,6 +3,16 @@
 import React from 'react';
 import '../Styles/landing.scss'
 import Header  from './Header';
+import Footer  from './Footer';
+import asset1 from '../images/asset1@2x.png';
+import asset2 from '../images/asset2@2x.png';
+import asset3 from '../images/asset3@2x.png';
+import asset4 from '../images/asset4@2x.png';
+
+/**
+ * This is the main landing page of our app
+ * @returns 
+ */
 
 export const Landing = ()=>{
     return (
@@ -14,18 +24,20 @@ export const Landing = ()=>{
 
             <main >
                 {/* Creating the landing header part of the landing page */}
+
                 <div className = "landing-heading">
                 <h1 className = "landing-header">Less stress when sharing expenses with anyone.</h1>
                 <p className = "landing-desc"><strong>Share</strong> bills. And make sure the bills are <strong>splitted in the right way</strong> and <strong>settled-up</strong>.</p>
                 <p className = "image-desc">
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset1@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset2@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset3@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
-                    <img src="https://www.splitwise.com/assets/home_page/fixtures/asset4@2x.png" height="600" width="375" aria-hidden alt="Image features"/>
+                    <img src={asset1} aria-hidden alt="Image features" height="600" width="375"/>
+                    <img src={asset2} aria-hidden alt="Image features" height="600" width="375"/>
+                    <img src={asset3} aria-hidden alt="Image features" height="600" width="375"/>
+                    <img src={asset4} aria-hidden alt="Image features" height="600" width="375"/>
                 </p>
                 
                 </div>
                 {/* Creating the landing feature of the landing page */}
+
                 <div className = "landing-feature">
                     <div className = "landing-content">
                         <h1>Splitting expenses are now a piece of cake.</h1> 
@@ -39,7 +51,9 @@ export const Landing = ()=>{
                         <a href="http://localhost:3000/signup">   <button className = "landing-button">  Get Started</button></a><br/><br/>
                     </div>
                 </div> 
-               
+                {/* Using the Footer for the landing page */}
+                
+               <Footer/>
             </main>
         </div>
     )
