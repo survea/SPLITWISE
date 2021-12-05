bb9dae1f55b1afe92ef1bd84478083dd6574c73d
surve.a <surve.a@northeastern.edu>
2021-12-04 20:20:20 -0500

    Updated header to display the person's profile

--- Webapp/src/components/Dashboard/DashboardHeaderComponent/DashHeader.js
+++ Webapp/src/components/Dashboard/DashboardHeaderComponent/DashHeader.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
 import {connect} from 'react-redux';
+import profile from '../../../images/person-profile.png';
 
 import "./DashHeader.scss"
  const DashHeader = (props)=>{
     
     return (
         <nav className = "DashNav fixedTop">
-        <NavLink to = "/Dashboard"><h3 className = "landing-name">E X P E N S E    T R A C K E R</h3></NavLink> 
+        <NavLink to = "/Dashboard"><h3 className = "landing-name">S P L I T W I S E</h3></NavLink> 
      
      <div className = "Dash-float">
      <NavLink to = "/login"><button className = "logout-btn" onClick = {()=>{
        localStorage.removeItem('jwtToken');
      }
      }>Log Out</button></NavLink>
+      <button className = "profileBtn" onClick={props.profile}>
+      <img className = "profile" src={profile} alt="" srcset=""/>
+      </button>
+      
+      <label htmlFor="">{props.user.username}</label>
       
      
       
