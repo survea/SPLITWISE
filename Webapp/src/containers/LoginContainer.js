19aee119ad03c6f71d23dcb514686a34270b0e67
NishithaSurendran <surendran.n@northeastern.edu>
2021-12-06 16:01:35 -0500

    login page updated

--- Webapp/src/containers/LoginContainer.js
+++ Webapp/src/containers/LoginContainer.js
@@ -13,20 +13,26 @@ import { withRouter } from "react-router-dom";
         this.input[event.target.id] = event.target.value;
     }
     Login(){
-    var pr = instance.post('/login',this.input);
-
-    pr.then((response)=>{
-        console.log(response.data);
-        const token = response.data.token;
-        localStorage.setItem('jwtToken',token)
-        this.props.history.push("/dashboard");
-        if(response.data.Status === 'S'){
-            this.props.history.push("/dashboard");
+        console.log("state of login");
+        console.log(this.input.email);
+        // validations added
+        if (this.input.email == undefined || this.input.password == undefined) {
+            alert("form is Incomplete");
+        } else {
+            var pr = instance.post('/login',this.input);
+            pr.then((response)=>{
+                console.log(response.data);
+                const token = response.data.token;
+                localStorage.setItem('jwtToken',token)
+                this.props.history.push("/dashboard");
+                if(response.data.Status === 'S'){
+                    this.props.history.push("/dashboard");
+                }
+                else if(response.data.Status === 'F'){
+                  this.setState({invalid:true});
+                }
+            })
         }
-        else if(response.data.Status === 'F'){
-          this.setState({invalid:true});
-        }
-    })
     }
     render(){
         return(
