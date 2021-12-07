8332a08a5e67053037434d7caf92757fbe5dc7cd
Diksha Godse <godse.d@northeastern.edu>
2021-12-07 10:52:37 -0500

    Delete expense - UI changes(integrated API call)

--- Webapp/src/components/Dashboard/ExpenseHistoryComponent/ExpenseTable.js
+++ Webapp/src/components/Dashboard/ExpenseHistoryComponent/ExpenseTable.js
@@ -1,40 +1,60 @@
 import React from "react";
 import { connect } from "react-redux";
 import './ExpenseTable.scss';
+import { instance } from '../../../utilities/AxiosConfig'
 
-  var selectedRow;
-  function addNewClick() {
+var selectedRow;
+function addNewClick() {
     const targetDiv = document.getElementById("expenseTableForm");
     targetDiv.style.display = "block";
-  }
-  function renderTableData(props) {
-    if(props.user.expensis){
+}
+async function removeData(rowData, props) {
+    // this.setState({ sendingEmail: true })
+    var pr = instance.post('/dashboard/deleteExpense', {
+        expense: rowData,
+        selectedUser: props.expenseTable,
+        loggedInUser: props.user
+    });
+    pr.then((response) => {
+        console.log(response.data.Status);
+        // if (response.data.msg == "Email sent, please check your inbox to confirm") {
+        //     alert(response.data.msg);
+        //     this.setState({ sendingEmail: false })
+        //     this.form.reset();
+        // } else {
+        //     alert(response.data.msg);
+        //     this.setState({ sendingEmail: false })
+        //     this.form.reset();
+        // }
+    })
+}
+function renderTableData(props) {
+    if (props.user.expensis) {
         var selectedUserData = props.user.expensis.find((element) => {
             return element.name == props.expenseTable;
-          })
-            return selectedUserData.data.map((rowData, index) => {
-                const { _id, desc, date, ammount } = rowData //destructuring
-                return (
-                  <tr key={_id}>
+        })
+        return selectedUserData.data.map((rowData, index) => {
+            const { _id, desc, date, ammount } = rowData //destructuring
+            return (
+                <tr key={_id}>
                     <td>{desc}</td>
                     <td>{date}</td>
                     <td>{ammount}</td>
-                    <td><a onClick={() => this.removeData(_id)}>Delete</a></td>
-                  </tr>
-                )
-             });
+                    <td><a onClick={() => removeData(rowData, props)}>Delete</a></td>
+                </tr>
+            )
+        });
+    }
 }
-  }
-  async function removeData(id) {
-  }
-  const ExpenseTable = props => {
+
+const ExpenseTable = props => {
     const clickedUser = props.expenseTable;
     return (
         <div>
-        {clickedUser != '' &&
-      
+            {clickedUser != '' &&
 
-        <><h1 className="title-styling"><center>Expense List</center></h1><div className="todo-table">
+
+                <><h1 className="title-styling"><center>Expense List</center></h1><div className="todo-table">
                     <table className="list" id="todoList">
                         <thead>
                             <tr>
@@ -49,16 +69,16 @@ import './ExpenseTable.scss';
                         </tbody>
                     </table>
                 </div><br /></>
-       }</div>
+            }</div>
 
     )
-  }
-  const mapStateToProps = state => {
+}
+const mapStateToProps = state => {
     console.log("state is  ", state);
     return {
-      user: state.user
+        user: state.user
     };
-  };
-  
-  const fn = connect(mapStateToProps);
-  export default fn(ExpenseTable);
+};
+
+const fn = connect(mapStateToProps);
+export default fn(ExpenseTable);
