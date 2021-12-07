85d313baefbc5605f1fc2ed986badf74a8ac1181
Diksha Godse <godse.d@northeastern.edu>
2021-12-07 17:01:55 -0500

    UI change - Added settlement table to UI for selected friend

--- Webapp/src/components/Dashboard/ExpenseHistoryComponent/ExpenseTable.js
+++ Webapp/src/components/Dashboard/ExpenseHistoryComponent/ExpenseTable.js
@@ -47,6 +47,24 @@ function renderTableData(user) {
         });
     }
 }
+function displaySettlementInfo(user) {
+    if (user.expensis) {
+        var selectedUserData = user.expensis.find((element) => {
+            return element.name == selectedUser;
+        })
+        return selectedUserData.settlementData.map((rowData, index) => {
+            const { _id, desc, date, ammount } = rowData //destructuring
+            return (
+                <tr key={_id}>
+                    <td>{desc}</td>
+                    <td>{date}</td>
+                    <td className={`${ammount > 0 ? "green" : "red"}`}>$ {ammount.toString().replace("-","")}</td>
+                    <td className = "red"><a onClick={() => removeData(rowData, user)}><i class="fas fa-trash"></i> Delete</a></td>
+                </tr>
+            )
+        });
+    }
+}
 
 const ExpenseTable = props => {
     const clickedUser = props.expenseTable;
@@ -68,6 +86,22 @@ const ExpenseTable = props => {
                         </thead>
                         <tbody>
                             {renderTableData(props.user)}
+                        </tbody>
+                    </table>
+                </div><br />
+                
+                <h1 className="title-styling"><center>Settlement List</center></h1><div className="todo-table">
+                    <table className="list" id="todoList">
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Date</th>
+                                <th>Amount</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {displaySettlementInfo(props.user)}
                         </tbody>
                     </table>
                 </div><br /></>
