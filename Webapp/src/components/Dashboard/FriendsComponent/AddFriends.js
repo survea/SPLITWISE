a73f2a130240d0ca9ed446ad1de74f83bffa036e
surve.a <surve.a@northeastern.edu>
2021-12-07 22:54:15 -0500

    Updated Profile update

--- Webapp/src/components/Dashboard/FriendsComponent/AddFriends.js
+++ Webapp/src/components/Dashboard/FriendsComponent/AddFriends.js
@@ -6,9 +6,6 @@ export const AddFriend = (props)=>{
         <div className = "AddFriendBox">
          
          <div className = "content">
-         <div class="input-prepend">
-            <span class="add-on"><i class="fa fa-search"><input class="span2" id="filter_search" type="text" placeholder="Filter by name"/></i></span>
-         </div>
             <div className = "friend">
                 <label htmlFor="">FRIENDS</label>
                 <button onClick = {props.friend} className = " addBtn float-right">+Add</button>
